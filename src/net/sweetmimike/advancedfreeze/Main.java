c8891c2f4a136d2b79966b33cd5cb59bc8a11879
Utilisateur <Utilisateur@DESKTOP-ALGE98B>
2018-08-31 16:18:02 +0200

    Creation du main

---
+++ src/net/sweetmimike/advancedfreeze/Main.java
@@ -0,0 +1,13 @@
+package net.sweetmimike.advancedfreeze;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+		super.onEnable();
+	}
+	
+}
